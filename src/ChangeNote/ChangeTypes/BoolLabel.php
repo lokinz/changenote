5ed3b2038d5d55f63d5364e65d9256a9d3e47050
logan <logan.clarke@gmail.com>
2021-06-26 01:07:00 +1200

    add BoolLabel change type

---
+++ src/ChangeNote/ChangeTypes/BoolLabel.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Harvest\ChangeNote\ChangeTypes;
+
+/** @Annotation  */
+class BoolLabel extends ChangeValue
+{
+
+    private $true;
+    private $false;
+
+    public function __construct(array $values)
+    {
+        $this->false = $values['false'] ?? 'FALSE';
+        $this->true = $values['true'] ?? 'TRUE';
+    }
+
+    public function getValue($value): string
+    {
+        $state = filter_var($value, FILTER_VALIDATE_BOOLEAN);
+
+        return $state ? $this->true : $this->false;
+    }
+}
